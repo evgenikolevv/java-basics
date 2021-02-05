5a280e2bd09bcf8b78b183c8a1445837c1aee726
evgenikolevv <ekoleww@gmail.com>
2021-02-05 11:47:59 +0200

    coins

--- SoftUni/src/while_loop_exercise/Coins.java
+++ SoftUni/src/while_loop_exercise/Coins.java
@@ -9,6 +9,33 @@ public class Coins {
         BigDecimal money = BigDecimal.valueOf(Double.parseDouble(console.nextLine()));
         int countCoins = 0;
 
-
+        while(money.compareTo(BigDecimal.valueOf(0.00))== 1){
+            if(money.compareTo(BigDecimal.valueOf(2.00)) >= 0){
+                money = money.subtract(BigDecimal.valueOf(2.00));
+                countCoins++;
+            }else if (money.compareTo(BigDecimal.valueOf(1.00)) >= 0){
+                money = money.subtract(BigDecimal.valueOf(1.00));
+                countCoins++;
+            }else if (money.compareTo(BigDecimal.valueOf(0.50)) >= 0){
+                money = money.subtract(BigDecimal.valueOf(0.50));
+                countCoins++;
+            }else if (money.compareTo(BigDecimal.valueOf(0.20)) >= 0){
+                money = money.subtract(BigDecimal.valueOf(0.20));
+                countCoins++;
+            }else if (money.compareTo(BigDecimal.valueOf(0.10)) >= 0){
+                money = money.subtract(BigDecimal.valueOf(0.10));
+                countCoins++;
+            }else if (money.compareTo(BigDecimal.valueOf(0.05)) >= 0){
+                money = money.subtract(BigDecimal.valueOf(0.05));
+                countCoins++;
+            }else if (money.compareTo(BigDecimal.valueOf(0.02)) >= 0){
+                money = money.subtract(BigDecimal.valueOf(0.02));
+                countCoins++;
+            }else if (money.compareTo(BigDecimal.valueOf(0.01)) >= 0){
+                money = money.subtract(BigDecimal.valueOf(0.01));
+                countCoins++;
+            }
+        }
+        System.out.println(countCoins);
     }
 }
